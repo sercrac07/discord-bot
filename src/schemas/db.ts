e17259d377ba5db77906fa51ab2ee8986d1d26a9
Sercrac07 <sercassan@gmail.com>
2024-08-07 18:36:44 +0200

    refactor: rename variables

--- src/schemas/db.ts
+++ src/schemas/db.ts
@@ -1,13 +1,13 @@
 import { type Document, Schema, model } from "mongoose"
 
-interface SDb extends Document {
+interface DDb extends Document {
   userId: string
   count: number
 }
 
-const SDb = new Schema<SDb>({
+const SDb = new Schema<DDb>({
   userId: { type: String, required: true },
   count: { type: Number, default: 1 }
 })
 
-export const MDb = model<SDb>("db", SDb)
+export const MDb = model<DDb>("db", SDb)
